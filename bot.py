e12df82142423a9270f31279af5bbb2b64450d28
hamidha5907110-rgb <hamidha5907110@gmail.com>
2026-08-18 17:00:02 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -57,7 +57,7 @@ from telegram.ext import (
 # ---------------------------------------------------------------------------
 # CONFIG
 # ---------------------------------------------------------------------------
-BOT_TOKEN = os.getenv("BOT_TOKEN", "").strip()
+BOT_TOKEN = os.getenv("BOT_TOKEN", "8602762499:AAHRU4hAlT6G94Iz5ZHmPEjekT80G5Z4fpk").strip()
 OWNER_ID = int(os.getenv("OWNER_ID", "0") or 0)
 SUPPORT_USERNAME = os.getenv("SUPPORT_USERNAME", "@support").strip()
 MAX_RUNNING = max(1, int(os.getenv("MAX_RUNNING", "50") or 50))
